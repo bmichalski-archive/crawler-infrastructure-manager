e9aa09aaf8e3325b536b24f233d30cf5dc917155
Benjamin Michalski <benjamin.michalski@gmail.com>
2016-10-08 17:26:23 +0200

    Updates crawlers:list.
    [crawlers/list.js]
    Fixes command to output table instead of text.
    
    [scaleway/images/list.js]
    Removes ;

--- src/crawlers/list.js
+++ src/crawlers/list.js
@@ -2,5 +2,37 @@
 
 const conf = require('./../../conf')
 const manager = require('@bmichalski/scaleway-infrastructure-manager')(conf)
+const Table = require('cli-table')
 
-manager.listServers(conf.servers.crawlers)
+manager
+  .listServers(conf.servers.crawlers)
+  .then((servers) => {
+    const table = new Table({
+      head: [ 'Name', 'Present', 'State', 'Id', 'Public IPv4', 'Type', 'Arch' ]
+    })
+
+    servers.forEach((server) => {
+      let rawData = [
+        server.name,
+        server.present
+      ]
+
+      if (undefined !== server.info) {
+        rawData = rawData.concat([
+          server.info.state,
+          server.info.id,
+          server.info.public_ip,
+          server.info.commercial_type,
+          server.info.arch
+        ])
+      }
+
+      const data = rawData.map((value) => {
+        return (undefined === value || null === value) ? '-' : value
+      })
+
+      table.push(data)
+    })
+
+    console.log(table.toString())
+  })
